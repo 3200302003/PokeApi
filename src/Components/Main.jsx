a38d3591fae960e4945b98b29b9735b23f18873d
FREDY QUEZADA <63796300+3200302003@users.noreply.github.com>
2022-06-29 19:59:22 -0500

    Chore: updating the Main.jsx

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 import Pokeinfo from "./Pokeinfo";
 import axios from "axios";
 import { useState } from "react";
-import { useEffect } from "react";
+
 const Main=()=>{
     const [pokeData,setPokeData]=useState([]);
     const [loading,setLoading]=useState(true);
@@ -13,16 +13,7 @@ const Main=()=>{
     const [pokeDex,setPokedex]=useState();
    
 
-    const pokeFun=async()=>{
-        setLoading(true)
-        const res=await axios.get(url);
-        //console.log(res.data.results);
-        setNextUrl(res.data.next);
-        setPrevUrl(res.data.previous);
-        getPokemon(res.data.results)
-        setLoading(false)
-        //console.log(pokeData)
-    }
+    
     const getPokemon=async(res)=>{
        res.map(async(item)=>{
            const result=await axios.get(item.url)
